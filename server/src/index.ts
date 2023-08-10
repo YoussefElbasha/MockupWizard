2008df8f5b6c19b12aca44ec52b095efb45222dc
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-08-10 10:43:02 +0300

    userRouter

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import * as bodyParser from 'body-parser'
+import { PrismaClient } from '@prisma/client'
+import  userRouter  from './routes/user'
 
 dotenv.config()
 const app = express()
@@ -9,6 +11,8 @@ const port = process.env.PORT
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use('/user',userRouter)
+
 
 
 app.listen(port, () => {
