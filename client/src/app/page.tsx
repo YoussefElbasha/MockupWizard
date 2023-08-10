f510da1c63d3f5303df06c8e776d2e1e815694de
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-08-10 15:48:13 +0300

    prettier format

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,31 +1,32 @@
-"use client" 
-import Image from 'next/image'
-import AuthCanvas from './components/auth-components/AuthCanvas'
-
+"use client";
+import Image from "next/image";
+import AuthCanvas from "./components/auth-components/AuthCanvas";
 
 export default function Home() {
   return (
     <div>
-      <div className='bg-blue-950 h-screen flex m-auto'>
-      <div className='m-auto items-center'>
-        <div className='flex items-center h-screen'>
-          <AuthCanvas />
-        <div className='p-10 text-white flex-grow justify-center flex flex-col items-start gap-10'>
-            <div>
-              <h1 className='text-4xl font-bold'>Unleash your products potential!</h1>
+      <div className="bg-blue-950 h-screen flex m-auto">
+        <div className="m-auto items-center">
+          <div className="flex items-center h-screen">
+            <AuthCanvas />
+            <div className="p-10 text-white flex-grow justify-center flex flex-col items-start gap-10">
+              <div>
+                <h1 className="text-4xl font-bold">
+                  Unleash your products potential!
+                </h1>
+              </div>
+              <div className="font-semibold">
+                <p>
+                  have a design you want to add? you can{" "}
+                  <span className="text-blue-700">
+                    <a href="/signup">Start Now!</a>
+                  </span>
+                </p>
+              </div>
             </div>
-            <div className='font-semibold'>
-              <p>
-               have a design you want to add? you can <span className='text-blue-700'><a href='/signup'>Start Now!</a></span>
-              </p>
-            </div>
+          </div>
         </div>
-            
-        </div>  
-       </div>
+      </div>
     </div>
-  </div>
-        
-
-  )
+  );
 }
