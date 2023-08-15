817cbfc72b5a72885029ad8f64ab6bced05642d5
sbayce <youssef2311@gmail.com>
2023-08-15 17:57:38 +0300

    welcome page styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,8 +5,16 @@ import AuthCanvas from "./components/auth-components/AuthCanvas";
 export default function Home() {
   return (
     <div>
-      <div className="bg-blue-950 h-screen flex m-auto">
-        <div className="m-auto items-center">
+      <div className="bg-[#14162E] h-screen flex m-auto relative">
+
+      <div className='absolute top-1/4 left-[6%] transform -translate-y-1/4'>
+        <div className='w-60 h-60 bg-[#DDA82A] rounded-full blur-3xl opacity-50' />
+      </div>
+      <div className='absolute top-1/2 left-1/4 transform translate-y-1/4 -translate-x-3/4'>
+        <div className='w-60 h-60 bg-[#4461F2] rounded-full blur-3xl opacity-50' />
+      </div>
+
+        <div className="m-auto items-center z-10">
           <div className="flex items-center h-screen">
             <AuthCanvas />
             <div className="p-10 text-white flex-grow justify-center flex flex-col items-start gap-10">
