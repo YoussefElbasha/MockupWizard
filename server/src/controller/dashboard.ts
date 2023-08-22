0027ab500c1f8de23fe9bbda1cafa9944e2c0046
sbayce <youssef2311@gmail.com>
2023-08-22 15:47:38 +0300

    create folder

--- server/src/controller/dashboard.ts
+++ server/src/controller/dashboard.ts
@@ -1,31 +1,35 @@
 import { Request, Response } from "express";
 
 const createFolder = async (req: Request, res: Response) => {
-  const { prisma } = req.context;
-  const { folderName } = req.body;
-  const userId = req.userId;
+  try {
+    const { prisma } = req.context;
+    const { folderName } = req.body;
+    const userId = req.userId;
 
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
 
-  const folder = await prisma.folder.findFirst({
-    where: { name: folderName },
-  });
+    const folder = await prisma.folder.findFirst({
+      where: { name: folderName },
+    });
 
-  if (folder) {
-    res.status(400).json("Folder already exists.");
-    return;
+    if (folder) {
+      return res.status(400).json("Folder already exists.");
+    }
+
+    await prisma.folder.create({
+      data: {
+        user: { connect: { id: userId } },
+        name: String(folderName),
+      },
+    });
+
+    return res.status(200).json("Folder created.");
+  } catch (e: any) {
+    console.log("Error creating folder:", e);
+    return res.status(500).json("An error occurred.");
   }
-
-  await prisma.folder.create({
-    data: {
-      user: { connect: { id: userId } },
-      name: String(folderName),
-    },
-  });
-
-  res.status(200).json("Folder created.");
 };
 
 export { createFolder };
