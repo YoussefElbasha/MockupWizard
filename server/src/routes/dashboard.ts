71d38cc91b07079d1b7075d04f825630a330d87f
sbayce <youssef2311@gmail.com>
2023-08-23 00:55:01 +0300

    get all folders

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 import { createFolder } from "../controller/dashboard/create-folder";
+import getAllFolders from "../controller/dashboard/get-all-folders";
 
 router.post("/create-folder", createFolder);
+router.get("/", getAllFolders);
 
 export default router;
