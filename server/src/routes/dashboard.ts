2c154c37da7b782cab3aa6e96bf8a8cb228c7e0a
thethirdsh <momenyasser52@gmail.com>
2023-08-23 14:07:20 +0300

    fixed delete design

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -5,9 +5,11 @@ const router = express.Router();
 import getAllFolders from "../controller/dashboard/get-all-folders";
 import createFolder from "../controller/dashboard/create-folder";
 import deleteFolder from "../controller/dashboard/delete-folder";
+import deleteDesign from "../controller/dashboard/delete-design";
 
 router.get("/", getAllFolders);
 router.post("/create-folder", createFolder);
 router.delete("/delete-folder/:folderId", deleteFolder);
+router.delete("/delete-design/:designId", deleteDesign);
 
 export default router;
