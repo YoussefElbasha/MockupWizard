72ef3f20cdbfa7e8df681d9cba1f2bda5a41d0a9
thethirdsh <momenyasser52@gmail.com>
2023-08-23 14:20:04 +0300

    Fixed delete project

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -5,9 +5,11 @@ const router = express.Router();
 import getAllFolders from "../controller/dashboard/get-all-folders";
 import createFolder from "../controller/dashboard/create-folder";
 import deleteFolder from "../controller/dashboard/delete-folder";
+import deleteProject from "../controller/dashboard/delete-project";
 
 router.get("/", getAllFolders);
 router.post("/create-folder", createFolder);
 router.delete("/delete-folder/:folderId", deleteFolder);
+router.delete("/delete-project/:projectId", deleteProject);
 
 export default router;
