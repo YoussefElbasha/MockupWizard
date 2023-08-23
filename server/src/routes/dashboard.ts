8d422351b3264a318d61f7b95b81dfa8eea50213
sbayce <youssef2311@gmail.com>
2023-08-23 16:23:10 +0300

    get folder contents

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -6,12 +6,14 @@ import getAllFolders from "../controller/dashboard/get-all-folders";
 import createFolder from "../controller/dashboard/create-folder";
 import deleteFolder from "../controller/dashboard/delete-folder";
 import deleteProject from "../controller/dashboard/delete-project";
-import getProjectContents from '../controller/dashboard/get-project-contents';
+import getProjectContents from "../controller/dashboard/get-project-contents";
+import getFolderContents from "../controller/dashboard/get-folder-contents";
 
 router.get("/", getAllFolders);
 router.post("/create-folder", createFolder);
 router.delete("/delete-folder/:folderId", deleteFolder);
 router.delete("/delete-project/:projectId", deleteProject);
 router.get("/get-project-contents/:projectId", getProjectContents);
+router.get("/get-folder-contents/:folderId", getFolderContents);
 
 export default router;
