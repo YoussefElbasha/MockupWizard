6e55ec35d20661c101b3f9ab7373d2697f87669f
thethirdsh <momenyasser52@gmail.com>
2023-08-23 14:28:07 +0300

    Fixed get project contents

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -5,9 +5,11 @@ const router = express.Router();
 import getAllFolders from "../controller/dashboard/get-all-folders";
 import createFolder from "../controller/dashboard/create-folder";
 import deleteFolder from "../controller/dashboard/delete-folder";
+import getProjectContents from '../controller/dashboard/get-project-contents';
 
 router.get("/", getAllFolders);
 router.post("/create-folder", createFolder);
 router.delete("/delete-folder/:folderId", deleteFolder);
+router.get("/get-project-contents/:projectId", getProjectContents);
 
 export default router;
