7a89a98385866692370f9ef25c429ff3c6f267a2
thethirdsh <momenyasser52@gmail.com>
2023-08-25 16:45:46 +0300

    .

---
+++ server/src/controller/dashboard/create-project.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+const createProject = async (req: Request, res: Response) => {
+  try {
+    const { prisma } = req.context;
+    const { name, modelId, folderId, thumbnail, screenshots } = req.body;
+    
+    await prisma.project.create({
+      data: {
+        folder: { connect: { id: String(folderId) } },
+        Model: { connect: { id: String(modelId) } },
+        modelId: String(modelId),
+        name: String(name),
+        thumbnail: thumbnail,
+        screenshots: screenshots,
+      },
+    });
+
+    return res.status(200).json("Project created successfully.");
+
+  } catch (error) {
+    console.log("Error creating project: ", error);
+    return res.status(500).json("An error occurred.");
+  }
+};
