ab88cca0c4151aabe461bfa079c3cbd43b51f6b4
thethirdsh <momenyasser52@gmail.com>
2023-08-26 01:50:36 +0300

    Implemented save editor

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -10,6 +10,7 @@ import deleteProject from "../controller/dashboard/delete-project";
 import getFolderContents from "../controller/dashboard/get-folder-contents";
 import getProjectContents from "../controller/dashboard/get-project-contents";
 import deleteDesign from "../controller/dashboard/delete-design";
+import saveEditor from "../controller/dashboard/save-editor";
 
 router.get("/", getAllFolders);
 router.post("/create-folder", createFolder);
@@ -19,5 +20,6 @@ router.delete("/delete-project/:projectId", deleteProject);
 router.get("/get-project-contents/:projectId", getProjectContents);
 router.get("/get-folder-contents/:folderId", getFolderContents);
 router.delete("/delete-design/:designId", deleteDesign);
+router.post("/save-editor", saveEditor)
 
 export default router;
