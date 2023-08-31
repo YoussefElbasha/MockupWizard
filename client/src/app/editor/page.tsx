31df802e20e4490e4b17a75545d7b0b0757a02f9
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-08-31 11:34:57 +0300

    changes

--- client/src/app/editor/page.tsx
+++ client/src/app/editor/page.tsx
@@ -4,11 +4,8 @@ import { useState } from 'react'
 import Editor from './components/Editor'
 import EditorTabs from './editor tabs/EditorTabs'
 import { FabricContextProvider } from './contexts/fabric-context'
-import FabricControls from './editor tabs/fabricControls'
-import Canvas from './components/Canvas'
 import TestCanvas from './components/testCanvas'
 import { CanvasContextProvider } from './contexts/canvas-context'
-import ControlsTabs from './editor tabs/ControlsTabs'
 
 const Home = () => {
   return (
