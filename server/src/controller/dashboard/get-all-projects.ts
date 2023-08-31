d58d40a47b0174dec97835a5619621ab087e82e1
thethirdsh <momenyasser52@gmail.com>
2023-08-31 15:19:39 +0300

    Updated get all projects

--- server/src/controller/dashboard/get-all-projects.ts
+++ server/src/controller/dashboard/get-all-projects.ts
@@ -3,15 +3,29 @@ import { Request, Response } from "express";
 const getAllProjects = async (req: Request, res: Response) => {
   try {
     const { prisma } = req.context;
-    const folderId = req.params.folderId;
+    const userId = req.userId;
 
-    const projects = await prisma.project.findMany({
-      where: { folderId: folderId },
+    const folders = await prisma.folder.findMany({
+      where: { userId: userId },
+      include: { projects: true }
     });
 
-    if (!projects) return res.status(400).json("No projects found.");
+    var projects: any[] = [] // this variable will contain arrays of projects
+    var singleProjects: any[] = [] // this variable will contain single projects not arrays
 
-    return res.status(200).json(projects);
+    for(const folder of folders) {
+      projects.push(...folder.projects)
+    }
+    
+    for(const p of projects) {
+      for(const project of p) {
+        singleProjects.push(project)
+      }
+    }
+
+    if (!singleProjects) return res.status(400).json("No projects found.");
+
+    return res.status(200).json(singleProjects);
   } catch (e: any) {
     console.log("Error getting all projects:", e);
     return res.status(500).json("An error occurred.");
