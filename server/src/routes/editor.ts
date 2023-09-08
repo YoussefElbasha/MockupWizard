af93f16a24f82a59868510c619932f2936978ddb
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-09-09 02:17:37 +0300

    changes

--- server/src/routes/editor.ts
+++ server/src/routes/editor.ts
@@ -2,6 +2,7 @@ import express from "express"
 
 import loadProject from "../controller/editor/load-project"
 import saveProject from "../controller/editor/save-project"
+import generateImageController from "../controller/editor/generate-image"
 // import deleteDesign from "../controller/editor/delete-design"
 // import editProject from "../controller/dashboard/edit-project";
 
@@ -9,6 +10,7 @@ const router = express.Router()
 
 router.get("/:projectId", loadProject)
 router.post('/:projectId', saveProject)
+router.post("/generate-image", generateImageController);
 // router.delete("/delete-design/:designId", deleteDesign)
 // router.put("/edit-project", editProject)
 
