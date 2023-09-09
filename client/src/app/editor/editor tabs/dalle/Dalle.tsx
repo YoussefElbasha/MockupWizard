42913c747688475a7f03e95fe971aef1bf5d86a2
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-09-09 19:21:49 +0300

    changes

--- client/src/app/editor/editor tabs/dalle/Dalle.tsx
+++ client/src/app/editor/editor tabs/dalle/Dalle.tsx
@@ -14,11 +14,14 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import useSWRMutation from 'swr/mutation'
+import { Save } from 'react-ionicons'
+import { useCanvasContext } from '../../contexts/canvas-context'
 
 const Dalle = () => {
   const [urls, setUrls] = useState<string[]>([])
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [selectedImage, setSelectedImage] = useState<string | null>(null)
+  const { setDesigns, canvasObjects } = useCanvasContext()
 
   const { trigger, isMutating } = useSWRMutation(
     'http://api.app.localhost:4000/editor/generate-image',
@@ -41,12 +44,43 @@ const Dalle = () => {
     setIsDialogOpen(false)
   }
 
-  const handleConfirm = () => {
+  const handleConfirm = async () => {
     // Perform actions when the user confirms their choice
-    if (selectedImage) {
-      console.log('Selected image:', selectedImage)
-    } else {
-      console.log('No image selected')
+    try {
+      if (selectedImage) {
+        const formData = new FormData()
+        formData.append(
+          'file',
+          'https://dalleproduse.blob.core.windows.net/private/images/dc435ca2-7d52-4793-8d48-61064cd5f370/generated_00.png?se=2023-09-10T16%3A08%3A04Z&sig=Pulj%2BWt9K0ZpPrrCuSqKJos64ZnG86SSpqBrBJxltAY%3D&ske=2023-09-16T12%3A43%3A34Z&skoid=09ba021e-c417-441c-b203-c81e5dcd7b7f&sks=b&skt=2023-09-09T12%3A43%3A34Z&sktid=33e01921-4d64-4f8c-a055-5bdaffd5e33d&skv=2020-10-02&sp=r&spr=https&sr=b&sv=2020-10-02'
+        )
+        formData.append('upload_preset', 'model_designs')
+
+        const res = await api.post(
+          'https://api.cloudinary.com/v1_1/dfbid2goy/image/upload',
+          formData
+        )
+
+        const path = res.data.url.replace(
+          'http://res.cloudinary.com/',
+          '/image/'
+        )
+
+        setDesigns((prev: any) => [
+          ...canvasObjects,
+          {
+            url: path,
+            top: 100,
+            left: 100,
+            scale: 100,
+            rotation: 0,
+          },
+        ])
+      } else {
+        console.log('No image selected')
+      }
+    } catch (error) {
+      toast.error('Something went wrong')
+      console.error(error)
     }
     setIsDialogOpen(false) // Close the dialog
   }
@@ -54,27 +88,11 @@ const Dalle = () => {
   const handleImageClick = (imageUrl: string) => {
     // Update the selected image when the user clicks on an image
     setSelectedImage(imageUrl)
-    console.log('Selected image:', selectedImage)
+    console.log('Selected image:', imageUrl)
   }
 
   return (
-    <div className="bg-background m-auto min-h-screen flex flex-col items-center justify-center relative">
-      <motion.div
-        initial={{ top: '21%', left: '6%', x: '0%', y: '0%', opacity: 1 }}
-        animate={{ top: '20%', left: '36%', x: '0%', y: '0%', opacity: 1 }}
-        transition={{ duration: 0.2, delay: 0 }}
-        className="absolute z-0"
-      >
-        <div className="w-60 h-60 bg-secondary rounded-full blur-3xl opacity-50" />
-      </motion.div>
-      <motion.div
-        initial={{ top: '57%', left: '13%', x: '0%', y: '0%', opacity: 1 }}
-        animate={{ top: '57%', left: '47%', x: '0%', y: '0%', opacity: 1 }}
-        transition={{ duration: 0.2, delay: 0 }}
-        className="absolute z-0"
-      >
-        <div className="w-60 h-60 bg-primary rounded-full blur-3xl opacity-50" />
-      </motion.div>
+    <div>
       <TextInput
         onSubmit={(prompt) => {
           setIsDialogOpen(true)
--- client/src/app/editor/editor tabs/dalle/components/TextInput.tsx
+++ client/src/app/editor/editor tabs/dalle/components/TextInput.tsx
@@ -1,59 +1,64 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+import React, { useState, useEffect, useRef } from 'react'
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { Planet } from 'react-ionicons'
 
 interface TextInputProps {
-  onSubmit: (prompt: string) => void;
+  onSubmit: (prompt: string) => void
 }
 
 const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
-  const [prompt, setPrompt] = useState("");
-  const [isVisible, setIsVisible] = useState(false);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [prompt, setPrompt] = useState('')
+  const [isVisible, setIsVisible] = useState(false)
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const handleTextareaChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
-    setPrompt(event.target.value);
-  };
+    setPrompt(event.target.value)
+  }
 
   const handleSubmit = () => {
-    onSubmit(prompt);
-    setIsVisible(false);
-  };
+    onSubmit(prompt)
+    setIsVisible(false)
+  }
 
   const handleClickOutside = (event: MouseEvent) => {
     if (
       containerRef.current &&
       !containerRef.current.contains(event.target as Node)
     ) {
-      setIsVisible(false);
+      setIsVisible(false)
     }
-  };
+  }
 
   const toggleTextInput = () => {
-    setIsVisible(!isVisible);
-  };
+    setIsVisible(!isVisible)
+  }
 
   useEffect(() => {
     // Add event listener when the component mounts
-    window.addEventListener("click", handleClickOutside);
+    window.addEventListener('click', handleClickOutside)
 
     // Remove event listener when the component unmounts
     return () => {
-      window.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
+      window.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
 
   return (
     <div className="relative" ref={containerRef}>
-      <Button
-        className="bg-primary rounded-full w-12 h-12 flex items-center justify-center text-white"
-        onClick={toggleTextInput}
-      >
-        Dalle
-      </Button>
+      <div>
+        <button
+          className="bg-white p-2 rounded-full w-[3.5em] h-[3.5em] drop-shadow-lg"
+          onClick={toggleTextInput}
+        >
+          <div className="translate-x-[0.20em] hover:scale-[1.1] transition-all ease-in-out duration-300">
+            <Planet style={{ fill: 'black', height: '2em', width: '2em' }} />
+          </div>
+        </button>
+      </div>
       {isVisible && (
         <div className="absolute bg-background rounded-lg shadow-md top-10 right-0 z-10 w-72 p-2">
           <Textarea
@@ -71,7 +76,7 @@ const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default TextInput;
+export default TextInput
