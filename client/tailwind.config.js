67cb0d27f2f5ec4e299e7c0528c30c348360d6de
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-09-09 19:39:23 +0300

    tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,13 +17,20 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#4461F2',
-        secondary: '#DDA82A',
-        background: '#14162E',
-        highlight: '#4461F21A',
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: "#4461F2",
+        secondary: "#DDA82A",
+        background: "#000212",
+        highlight: "#4461F21A",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        backgroundImage: { "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "custom-gradient":
@@ -32,6 +39,42 @@ module.exports = {
           "radial-gradient(70.71% 70.71% at 50% 50%, #DDA82A80 0%, #DDA82A40 30%, rgba(0, 0, 0, 0.00) 70%)",
         "custom-radial-gradient2":
           "radial-gradient(70.71% 70.71% at 50% 50%, #DDA82A80 0%, #4461F2 20%, rgba(0, 0, 0, 0.00) 65%)",
+      },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
