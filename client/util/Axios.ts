75f7a1227e2da0283e4a125235678ce5ed86eb23
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-09-10 13:23:42 +0300

    fix2

--- client/util/Axios.ts
+++ client/util/Axios.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   withCredentials: true,
-  headers: {
-    "Access-Control-Allow-Origin": process.env.FRONTEND_APP_URL,
-  },
 });
 
 export default api;
