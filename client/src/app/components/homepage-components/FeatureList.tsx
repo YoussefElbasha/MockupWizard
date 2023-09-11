b26b04b48fe669f8d3a7ea3b0a33907666eff5a5
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-09-12 00:15:40 +0300

    fixed screenshot aspect ratio

--- client/src/app/components/homepage-components/FeatureList.tsx
+++ client/src/app/components/homepage-components/FeatureList.tsx
@@ -4,25 +4,25 @@ import FeatureItem from './FeautureItem'
 const features = [
   {
     title: 'Effortlessly Visualize Your Designs',
-    imgUrl: '/feature screenshots/1.png',
+    imgUrl: '/feature screenshots/first.png',
     paragraph:
       'Transform your design ideas into captivating visuals with Mockup Wizard. Our intuitive platform allows you to bring your designs to life on t-shirts and mugs, helping you visualize how they will look in the real world.',
   },
   {
     title: 'AI-Powered Design Generation',
-    imgUrl: '/feature screenshots/2.png',
+    imgUrl: '/feature screenshots/second.png',
     paragraph:
       "Experience the power of AI with Mockup Wizard's cutting-edge design assistance feature. Let the AI algorithm fuel your creativity, offering fresh perspectives and inspiring ideas that will take your designs to the next level.",
   },
   {
     title: 'Manage and Categorize Your Designs',
-    imgUrl: '/feature screenshots/3.png',
+    imgUrl: '/feature screenshots/third.png',
     paragraph:
       "Keep your design projects organized and easily accessible with Mockup Wizard's folder organization feature. Say goodbye to scattered files and endless searches. Our intuitive platform allows you to streamline your design workflow by creating folders for different clients, campaigns, or personal projects. Stay on top of your work and efficiently manage multiple designs with ease.",
   },
   {
     title: 'Endless Possibilities for Customization',
-    imgUrl: '/feature screenshots/4.png',
+    imgUrl: '/feature screenshots/fourth.png',
     paragraph:
       "Our platform offers a wide range of customization options, giving you the freedom to create truly unique products. Choose from a diverse selection of mugs and t-shirts, with options for different colors, sizes, and materials. Whether you're designing for yourself, your brand, or for gifting purposes, our customization options ensure that your creations stand out from the crowd.",
   },
