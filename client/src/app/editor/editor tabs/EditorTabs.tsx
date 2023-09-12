309f59daac41ae310bedd33b48319232feacd416
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-09-12 12:33:41 +0300

    fixed type error

--- client/src/app/editor/editor tabs/EditorTabs.tsx
+++ client/src/app/editor/editor tabs/EditorTabs.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import React, { useState } from 'react'
-import { ChevronBack, ChevronBackCircle, ColorPalette, Images, Rocket } from 'react-ionicons'
+import {
+  ChevronBack,
+  ChevronBackCircle,
+  ColorPalette,
+  Images,
+  Rocket,
+} from 'react-ionicons'
 import TabButton from './TabButton'
 import ColorPicker from './ColorPicker'
 import FileUpload from './FileUpload'
@@ -27,7 +33,10 @@ const EditorTabs = forwardRef<HTMLCanvasElement, EditorTabsProps>(
 
     return (
       <>
-        <Link href="/dashboard" className="absolute w-10 h-10 p-2 transition-all duration-300 ease-in-out bg-white rounded-full top-6 left-6 hover:bg-neutral-200/60">
+        <Link
+          href="/dashboard"
+          className="absolute w-10 h-10 p-2 transition-all duration-300 ease-in-out bg-white rounded-full top-6 left-6 hover:bg-neutral-200/60"
+        >
           <ChevronBack cssClasses="!w-full !h-full" />
         </Link>
         <div className="absolute z-10 flex flex-col gap-3 -translate-y-1/2 top-1/2 left-6">
@@ -74,7 +83,8 @@ const EditorTabs = forwardRef<HTMLCanvasElement, EditorTabsProps>(
                   className="bg-white p-2 rounded-full w-[3.5em] h-[3.5em] drop-shadow-lg"
                   onClick={() =>
                     activeTab !== 3 ? setActiveTab(3) : setActiveTab(0)
-                  }>
+                  }
+                >
                   <p className="sr-only">Model Picker</p>
                   <div className="translate-x-[0.20em] hover:scale-[1.1] transition-all ease-in-out duration-300">
                     <Rocket
@@ -95,7 +105,7 @@ const EditorTabs = forwardRef<HTMLCanvasElement, EditorTabsProps>(
             <DownloadModelButton ref={props.meshRef} />
           </div>
           <div className="relative">
-            <SaveButton projectId={props.projectId} />
+            <SaveButton projectId={props.projectId} meshRef={props.meshRef} />
           </div>
         </div>
       </>
--- client/src/app/editor/editor tabs/SaveButton.tsx
+++ client/src/app/editor/editor tabs/SaveButton.tsx
@@ -3,51 +3,52 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "../../../components/Hover";
-import axios from "axios";
-import { Save } from "react-ionicons";
-import useSWRMutation from "swr/mutation";
-import { useCanvasContext } from "../contexts/canvas-context";
-import Loader from "@/components/loader";
-import { useEffect, useRef, useState } from "react";
-import Editor from "../components/Editor";
+} from '../../../components/Hover'
+import axios from 'axios'
+import { Save } from 'react-ionicons'
+import useSWRMutation from 'swr/mutation'
+import { useCanvasContext } from '../contexts/canvas-context'
+import Loader from '@/components/loader'
+import { useEffect, useRef, useState } from 'react'
+import Editor from '../components/Editor'
 // @ts-ignore
-import { useScreenshot } from "use-react-screenshot";
-import toast from "react-hot-toast";
-import api from "../../../../util/Axios";
+import { useScreenshot } from 'use-react-screenshot'
+import toast from 'react-hot-toast'
+import api from '../../../../util/Axios'
 
 interface saveButtonProps {
-  projectId: string;
+  projectId: string
+  meshRef: React.MutableRefObject<any>
 }
 
-const SaveButton = ({ projectId }: saveButtonProps) => {
-  const { color, canvasObjects, modelType } = useCanvasContext();
-  const [isSaving, setIsSaving] = useState<boolean>(false);
-  const [_, takeScreenshot] = useScreenshot();
-  const screenshotContainerRef = useRef<HTMLDivElement>(null);
+const SaveButton = ({ projectId, meshRef }: saveButtonProps) => {
+  const { color, canvasObjects, modelType } = useCanvasContext()
+  const [isSaving, setIsSaving] = useState<boolean>(false)
+  const [_, takeScreenshot] = useScreenshot()
+  const screenshotContainerRef = useRef<HTMLDivElement>(null)
 
   const handleSave = () => {
-    setIsSaving(true);
-  };
+    setIsSaving(true)
+  }
 
   useEffect(() => {
-    if (!isSaving) return;
-    if (!screenshotContainerRef.current) return;
+    if (!isSaving) return
+    if (!screenshotContainerRef.current) return
 
     const timeout = setTimeout(async () => {
-      const image = await takeScreenshot(screenshotContainerRef.current);
+      const image = await takeScreenshot(screenshotContainerRef.current)
 
-      const formData = new FormData();
-      formData.append("file", image);
-      formData.append("upload_preset", "model_designs");
+      const formData = new FormData()
+      formData.append('file', image)
+      formData.append('upload_preset', 'model_designs')
 
       try {
         const {
           data: { secure_url: url },
         } = await axios.post(
-          "https://api.cloudinary.com/v1_1/dfbid2goy/image/upload",
+          'https://api.cloudinary.com/v1_1/dfbid2goy/image/upload',
           formData
-        );
+        )
 
         await api.post(
           `${process.env.NEXT_PUBLIC_API_URL}/editor/${projectId}`,
@@ -57,19 +58,19 @@ const SaveButton = ({ projectId }: saveButtonProps) => {
             modelType,
             thumbnail: url,
           }
-        );
+        )
 
-        toast.success("Design saved");
+        toast.success('Design saved')
       } catch (error) {
-        toast.error("Failed to save design");
+        toast.error('Failed to save design')
       } finally {
-        setIsSaving(false);
+        setIsSaving(false)
       }
-    }, 1000);
+    }, 1000)
 
-    return () => clearTimeout(timeout);
+    return () => clearTimeout(timeout)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSaving, screenshotContainerRef]);
+  }, [isSaving, screenshotContainerRef])
 
   return (
     <TooltipProvider delayDuration={300}>
@@ -84,7 +85,7 @@ const SaveButton = ({ projectId }: saveButtonProps) => {
               <div className="translate-x-[0.20em] hover:scale-[1.1] transition-all ease-in-out duration-300 ">
                 {!isSaving ? (
                   <Save
-                    style={{ fill: "black", height: "2em", width: "2em" }}
+                    style={{ fill: 'black', height: '2em', width: '2em' }}
                   />
                 ) : (
                   <div className="w-full -translate-x-1.5 h-full flex items-center justify-center">
@@ -98,7 +99,7 @@ const SaveButton = ({ projectId }: saveButtonProps) => {
                 ref={screenshotContainerRef}
                 className="fixed left-[100vw] w-[1000px] h-[1000px] bg-red-400 -top-52"
               >
-                <Editor id="save-canvas-editor" />
+                <Editor id="save-canvas-editor" meshRef={meshRef} />
               </div>
             )}
           </div>
@@ -113,6 +114,6 @@ const SaveButton = ({ projectId }: saveButtonProps) => {
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
-  );
-};
-export default SaveButton;
+  )
+}
+export default SaveButton
