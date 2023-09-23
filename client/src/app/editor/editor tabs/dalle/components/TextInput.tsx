7675927cf86b0e644c2e4afb879370cabd48a437
ElBarBary01 <omar.m.elbarbary@gmail.com>
2023-09-23 20:29:47 +0300

    Disable the image generation

--- client/src/app/editor/editor tabs/dalle/components/TextInput.tsx
+++ client/src/app/editor/editor tabs/dalle/components/TextInput.tsx
@@ -3,56 +3,57 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '../../../../../components/Hover'
-import React, { useState, useEffect, useRef } from 'react'
-import { Button } from '@/components/ui/button'
-import { Textarea } from '@/components/ui/textarea'
-import { Planet } from 'react-ionicons'
+} from "../../../../../components/Hover";
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Planet } from "react-ionicons";
+import WizardIcon from "../../../../icons/wizard.svg";
 
 interface TextInputProps {
-  onSubmit: (prompt: string) => void
+  onSubmit: (prompt: string) => void;
 }
 
 const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
-  const [prompt, setPrompt] = useState('')
-  const [isVisible, setIsVisible] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
+  const [prompt, setPrompt] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleTextareaChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
-    setPrompt(event.target.value)
-  }
+    setPrompt(event.target.value);
+  };
 
   const handleSubmit = () => {
-    onSubmit(prompt)
-    setIsVisible(false)
-  }
+    onSubmit(prompt);
+    setIsVisible(false);
+  };
 
   const handleClickOutside = (event: MouseEvent) => {
     if (
       containerRef.current &&
       !containerRef.current.contains(event.target as Node)
     ) {
-      setIsVisible(false)
+      setIsVisible(false);
     }
-  }
+  };
 
   const toggleTextInput = () => {
-    setIsVisible(!isVisible)
-  }
+    setIsVisible(!isVisible);
+  };
 
   useEffect(() => {
     // Add event listener when the component mounts
-    window.addEventListener('click', handleClickOutside)
+    window.addEventListener("click", handleClickOutside);
 
     // Remove event listener when the component unmounts
     return () => {
-      window.removeEventListener('click', handleClickOutside)
-    }
-  }, [])
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="relative" ref={containerRef}>
@@ -60,18 +61,21 @@ const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
         <Tooltip
           open={isOpen}
           onOpenChange={(open: boolean) => {
-            if (isVisible) setIsOpen(false)
-            else setIsOpen(open)
-          }}>
+            if (isVisible) setIsOpen(false);
+            else setIsOpen(open);
+          }}
+        >
           <TooltipTrigger asChild>
             <div>
               <button
-                className="bg-white p-2 rounded-full w-[3.5em] h-[3.5em] drop-shadow-lg"
-                onClick={toggleTextInput}>
+                className="bg-gray-400 p-2 rounded-full w-[3.5em] h-[3.5em] drop-shadow-lg "
+                disabled
+                onClick={toggleTextInput}
+              >
                 <p className="sr-only">Open text input</p>
                 <div className="translate-x-[0.20em] hover:scale-[1.1] transition-all ease-in-out duration-300">
                   <Planet
-                    style={{ fill: 'black', height: '2em', width: '2em' }}
+                    style={{ fill: "black", height: "2em", width: "2em" }}
                   />
                 </div>
               </button>
@@ -81,7 +85,8 @@ const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
             sideOffset={-45}
             alignOffset={75}
             align="start"
-            avoidCollisions={false}>
+            avoidCollisions={false}
+          >
             <p>Generate Image from Text</p>
           </TooltipContent>
         </Tooltip>
@@ -96,13 +101,14 @@ const TextInput: React.FC<TextInputProps> = ({ onSubmit }) => {
           />
           <Button
             className="w-full px-4 py-2 mt-2 text-white rounded-lg bg-secondary"
-            onClick={handleSubmit}>
+            onClick={handleSubmit}
+          >
             Submit
           </Button>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default TextInput
+export default TextInput;
